e7911595b2fafd80951677b46eacf4f498d91fff
Rohit <167241336+officalrohit@users.noreply.github.com>
2026-06-22 15:02:49 +0530

    Remove CompoundInterest function and related variables

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let A;
-const P = 100000;
-const r = 0.05;
-const n = 1;
-const t = 3;
 
-function CompoundInterest() {
-  return A = P * (1 + (r / n)) ** (n * t);
-}
-
-console.log(CompoundInterest());
